4279e60147ef31eaa40566faf854875d73356df1
Ago1702 <davide.agostini1702@gmail.com>
2025-02-19 03:00:56 +0100

    Codice attore + alcuni test con torchrl

--- marl/model.py
+++ marl/model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 class MultiHeadActor(nn.Module):
 
-    def __init__(self, num_in:int, num_out:int, hidden:int, heads:int):
+    def __init__(self, num_in:int, num_out:int, hidden:int = 2, heads:int = 1, mean:float = 0, std:float=0.4):
         super(MultiHeadActor, self).__init__()
         self.heads = heads
         self.num_out = num_out
@@ -13,14 +13,31 @@ class MultiHeadActor(nn.Module):
 
         self.out = nn.Linear(hidden, num_out*heads)
 
-    def forward(self, X) -> torch.Tensor:
-        tmp = X
-        tmp = nn.ReLU(self.lin1(tmp))
-        tmp = nn.ReLU(self.lin2(tmp))
-        return nn.Sigmoid(self.out(tmp))
+        #Parametri rumore
+        self.mean = mean
+        self.std = std
+        self.noise = torch.Tensor(num_out * heads)
 
-    def action(self, state:torch.Tensor, head:int=-1):
+        self.apply(weights_init)
+
+    def forward(self, X:torch.Tensor) -> torch.Tensor:
+        #Uso Tanh visto che spesso viene utilizzata in ambito di RL
+        if(len(X.shape) < 2):
+            X = X.unsqueeze(0)
+        tmp = X
+        tmp = torch.tanh(self.lin1(tmp))
+        tmp = torch.tanh(self.lin2(tmp))
+        return torch.tanh(self.out(tmp))
+
+    def action(self, state:torch.Tensor, head:int=-1) -> torch.Tensor:
         X = self.forward(state)
+        return self.head_select(X, head)
+    
+    def noise_action(self, state:torch.Tensor, head:int=-1) -> torch.Tensor:
+        X = self.forward(state) + self.noise.normal_(self.mean, self.std)
+        return self.head_select(X, head)
+    
+    def head_select(self, X:torch.Tensor, head:int=-1) -> torch.Tensor:
         if head != -1:
             start = head * self.num_out
             return X[:, start:start + self.num_out]
@@ -31,3 +48,17 @@ def weights_init(m):
     if classname.find('Linear') != -1:
         torch.nn.init.xavier_normal_(m.weight, gain=0.5)
         torch.nn.init.constant_(m.bias, 0)
+
+'''
+if __name__ == "__main__":
+    actor = MultiHeadActor(5, 10, heads=3)
+    state = torch.Tensor(5).normal_(0,1)
+    print(state.shape)
+    print(state)
+    res = actor.action(state, 2)
+    print(res)
+    res = actor.action(state, 2)
+    print(res)
+    res = actor.noise_action(state, 2)
+    print(res)
+    res = actor(state)'''
--- marl/test.py
+++ marl/test.py
@@ -1,5 +1,6 @@
-PRINT = False
+PRINT=True
 
+import os
 import copy
 import tempfile
 
@@ -12,7 +13,7 @@ from tensordict.nn import TensorDictModule, TensorDictSequential
 from torch import multiprocessing
 
 from torchrl.collectors import SyncDataCollector
-from torchrl.data import LazyMemmapStorage, RandomSampler, ReplayBuffer
+from torchrl.data import LazyTensorStorage, RandomSampler, ReplayBuffer
 
 from torchrl.envs import (
     check_env_specs,
@@ -47,25 +48,24 @@ seed = 2025
 torch.manual_seed(seed)
 
 
-is_fork = multiprocessing.get_start_method() == 'fork'
+is_fork = multiprocessing.get_start_method() == "fork"
 device = (
     torch.device(0)
     if torch.cuda.is_available() and not is_fork
-    else torch.device('cpu')
+    else torch.device("cpu")
 )
-print('',"+++" + str(device) + "+++",'')
 
-frames_per_batch = 1_000
-n_iters = 10
+frames_per_batch = 48_000
+n_iters = 100
 total_frames = frames_per_batch * n_iters
 
-iteration_when_stop_training_evaders = n_iters // 2
+iteration_when_stop_training_evaders = n_iters // 3 * 2
 
 memory_size = 1_000_000
 
-n_optimizer_step = 100
-tain_batch_size = 128
-lr = 3e-4
+n_optimiser_steps = 100
+train_batch_size = 1024
+lr = 3e-4  # Learning rate
 max_grad_norm = 1.0
 
 gamma = 0.99
@@ -73,13 +73,13 @@ polyak_tau = 0.005
 
 ### ENV ###
 
-max_steps = 100
+max_steps = 100  # Environment steps before done
 
 n_chasers = 2
 n_evaders = 1
 n_obstacles = 2
 
-use_vmas = False
+use_vmas = True
 
 if not use_vmas:
     kwargs = {  #Scenario Spec
@@ -93,7 +93,8 @@ if not use_vmas:
         task='simple_tag_v3',
         parallel=True,
         seed=seed,
-        **kwargs
+        **kwargs,
+        device=device,
     )
 else:
     num_vmas_envs = (
@@ -111,7 +112,7 @@ else:
         max_steps=max_steps,
         device=device,
         seed=seed,
-        **kwargs
+        **kwargs,
     )
 
 if PRINT:
@@ -139,3 +140,296 @@ rollout = env.rollout(n_rollout_steps)
 if PRINT:
     print(f"rollout of {n_rollout_steps} steps:", rollout)
     print("Shape of the rollout TensorDict:", rollout.batch_size)
+
+#NN definition
+
+policy_modules = {}
+for group, agents in env.group_map.items():
+    share_parameters_policy = True
+    policy_net = MultiAgentMLP(
+        n_agent_inputs=env.observation_spec[group, "observation"].shape[
+            -1
+        ],
+        n_agent_outputs=env.full_action_spec[group, "action"].shape[-1],
+        n_agents=len(agents),
+        centralised=False,
+        share_params=share_parameters_policy,
+        device=device,
+        depth=2,
+        num_cells=256,
+        activation_class=torch.nn.Tanh,
+    )
+
+    policy_module = TensorDictModule(
+        policy_net,
+        in_keys=[(group, "observation")],
+        out_keys=[(group, "param")],
+    )
+
+    policy_modules[group] = policy_module
+
+policies = {}
+
+for group, _agents in env.group_map.items():
+    policy = ProbabilisticActor(
+        module=policy_modules[group],
+        spec=env.full_action_spec[group, "action"],
+        in_keys=[(group, "param")],
+        out_keys=[(group, "action")],
+        distribution_class=TanhDelta,
+        distribution_kwargs={
+            "low": env.full_action_spec[group, "action"].space.low,
+            "high": env.full_action_spec[group, "action"].space.high,
+        },
+        return_log_prob=False,
+    )
+    policies[group] = policy
+
+exploration_policies = {}
+for group, _agents in env.group_map.items():
+    exploration_policy = TensorDictSequential(
+        policies[group],
+        AdditiveGaussianModule(
+            spec=policies[group].spec,
+            annealing_num_steps=total_frames//2,
+            action_key=(group, 'action'),
+            sigma_init=0.9,
+            sigma_end=0.1,
+        ),
+    )
+    exploration_policies[group] = exploration_policy
+
+##Critic Network
+
+critics = {}
+
+for group, agents in env.group_map.items():
+    share_parameters_critic = True
+    MADDPG = True
+
+    cat_module = TensorDictModule(
+        lambda obs, action: torch.cat([obs, action], dim=-1),
+        in_keys=[(group, "observation"), (group, "action")],
+        out_keys=[(group, "obs_action")],
+    )
+
+    critic_module = TensorDictModule(
+        module=MultiAgentMLP(
+            n_agent_inputs=env.observation_spec[group, "observation"].shape[-1]
+            + env.full_action_spec[group, "action"].shape[-1],
+            n_agent_outputs=1,  # 1 value per agent
+            n_agents=len(agents),
+            centralised=MADDPG,
+            share_params=share_parameters_critic,
+            device=device,
+            depth=2,
+            num_cells=256,
+            activation_class=torch.nn.Tanh,
+        ),
+        in_keys=[(group, "obs_action")],
+        out_keys=[
+            (group, "state_action_value")
+        ],  # Write ``(group, "state_action_value")``
+    )
+
+    critics[group] = TensorDictSequential(
+        cat_module, critic_module
+    )  # Run them in sequence
+
+reset_td = env.reset()
+for group, _agents in env.group_map.items():
+    print(
+        f"Running value and policy for group '{group}':",
+        critics[group](policies[group](reset_td)),
+    )
+# Put exploration policies from each group in a sequence
+agents_exploration_policy = TensorDictSequential(*exploration_policies.values())
+
+collector = SyncDataCollector(
+    env,
+    agents_exploration_policy,
+    device=device,
+    frames_per_batch=frames_per_batch,
+    total_frames=total_frames,
+)
+
+replay_buffers = {}
+for group, _agents in env.group_map.items():
+    replay_buffer = ReplayBuffer(
+        storage=LazyTensorStorage(
+            memory_size, device=device
+        ),
+        sampler=RandomSampler(),
+        batch_size=train_batch_size,
+    )
+    replay_buffers[group] = replay_buffer
+
+losses = {}
+
+for group, _agents in env.group_map.items():
+    loss_module = DDPGLoss(
+        actor_network=policies[group],
+        value_network=critics[group],
+        delay_value=True,
+        loss_function="l2",
+
+    )
+    loss_module.set_keys(
+        state_action_value=(group, "state_action_value"),
+        reward=(group, "reward"),
+        done=(group, "done"),
+        terminated=(group, "terminated"),
+    )
+    loss_module.make_value_estimator(ValueEstimators.TD0, gamma=gamma)
+
+    losses[group] = loss_module
+
+target_updaters = {
+    group: SoftUpdate(loss, tau=polyak_tau) for group, loss in losses.items()
+}
+
+optimisers = {
+    group: {
+        "loss_actor": torch.optim.Adam(
+            loss.actor_network_params.flatten_keys().values(), lr=lr
+        ),
+        "loss_value": torch.optim.Adam(
+            loss.value_network_params.flatten_keys().values(), lr=lr
+        ),
+    }
+    for group, loss in losses.items()
+}
+
+def process_batch(batch: TensorDictBase) -> TensorDictBase:
+    for group in env.group_map.keys():
+        keys = list(batch.keys(True, True))
+        group_shape = batch.get_item_shape(group)
+        nested_done_key = ("next", group, "done")
+        nested_terminated_key = ("next", group, "terminated")
+        if nested_done_key not in keys:
+            batch.set(
+                nested_done_key,
+                batch.get(("next", "done")).unsqueeze(-1).expand((*group_shape, 1)),
+            )
+        if nested_terminated_key not in keys:
+            batch.set(
+                nested_terminated_key,
+                batch.get(("next", "terminated"))
+                .unsqueeze(-1)
+                .expand((*group_shape, 1)),
+            )
+    return batch
+
+pbar = tqdm(
+    total=n_iters,
+    desc=", ".join(
+        [f"episode_reward_mean_{group} = 0" for group in env.group_map.keys()]
+    ),
+)
+episode_reward_mean_map = {group: [] for group in env.group_map.keys()}
+train_group_map = copy.deepcopy(env.group_map)
+
+for iteration, batch in enumerate(collector):
+    current_frames = batch.numel()
+    batch = process_batch(batch)
+    for group in train_group_map.keys():
+        group_batch = batch.exclude(
+            *[
+                key for _group in env.group_map.keys()
+                if _group != group
+                for key in [_group, ('next', _group)]
+            ]
+        )
+        group_batch = group_batch.reshape(-1)
+        replay_buffers[group].extend(group_batch)
+
+        for _ in range(n_optimiser_steps):
+            subdata = replay_buffers[group].sample()
+            loss_vals = losses[group](subdata)
+
+            for loss_name in ["loss_actor", "loss_value"]:
+                loss = loss_vals[loss_name]
+                optimiser = optimisers[group][loss_name]
+
+                loss.backward()
+
+                params = optimiser.param_groups[0]["params"]
+                torch.nn.utils.clip_grad_norm_(params, max_grad_norm)
+
+                optimiser.step()
+                optimiser.zero_grad()
+            
+            target_updaters[group].step()
+        
+        exploration_policies[group][-1].step(current_frames)
+
+    if iteration == iteration_when_stop_training_evaders:
+        del train_group_map['agent']
+    
+    for group in env.group_map.keys():
+        episode_reward_mean = (
+            batch.get(('next', group, "episode_reward"))[
+                batch.get(('next', group, 'done'))
+            ].mean().item()
+        )
+        episode_reward_mean_map[group].append(episode_reward_mean)
+    
+    pbar.set_description(
+        ", ".join(
+            [
+                f"episode_reward_mean_{group} = {episode_reward_mean_map[group][-1]:.4}"
+                for group in env.group_map.keys()
+            ]
+        ),
+        refresh=False
+    )
+    pbar.update()
+
+fig, axs = plt.subplots(2, 1)
+for i, group in enumerate(env.group_map.keys()):
+    axs[i].plot(episode_reward_mean_map[group], label=f"Episode reward mean {group}")
+    axs[i].set_ylabel("Reward")
+    axs[i].axvline(
+        x=iteration_when_stop_training_evaders,
+        label="Agent (evader) stop training",
+        color="orange",
+    )
+    axs[i].legend()
+axs[-1].set_xlabel("Training iterations")
+plt.savefig("plot.png")
+
+#with torch.no_grad():
+#    env.rollout(
+#        max_steps=max_steps,
+#        policy=agents_exploration_policy,
+#        callback=lambda env, _: env.render(),
+#        auto_cast_to_device=True,
+#        break_when_any_done=True,
+#    )
+
+if use_vmas and not is_sphinx:
+    # Replace tmpdir with any desired path where the video should be saved
+    tmpdir = "./tmp/video"
+    video_logger = CSVLogger("vmas_logs", tmpdir, video_format="mp4")
+    print("Creating rendering env")
+    env_with_render = TransformedEnv(env.base_env, env.transform.clone())
+    env_with_render = env_with_render.append_transform(
+        PixelRenderTransform(
+            out_keys=["pixels"],
+            # the np.ndarray has a negative stride and needs to be copied before being cast to a tensor
+            preproc=lambda x: x.copy(),
+            as_non_tensor=True,
+            # asking for array rather than on-screen rendering
+            mode="rgb_array",
+        )
+    )
+    env_with_render = env_with_render.append_transform(
+        VideoRecorder(logger=video_logger, tag="vmas_rendered", skip=1)
+    )
+    with set_exploration_type(ExplorationType.DETERMINISTIC):
+        print("Rendering rollout...")
+        env_with_render.rollout(100, policy=agents_exploration_policy, break_when_any_done=True)
+    print("Saving the video...")
+    env_with_render.transform.dump()
+    print("Saved! Saved directory tree:")
+    video_logger.print_log_dir()
